9fbc5cfc15da4e6bd571364f96e0d5ef2155eeb6
louis <automatwon@gmail.com>
2019-03-16 01:56:28 -0400

    auth: google sign-in and send auth code to server

--- cetea.go
+++ cetea.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io"
 	"net/http"
 	"time"
 )
@@ -11,8 +14,13 @@ type Welcome struct {
 	Time string
 }
 
+type AuthorizationStruct struct {
+	Code string
+}
+
 func main() {
 	http.HandleFunc("/", mainhandler)
+	http.HandleFunc("/authorization", authorization)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
@@ -28,4 +36,31 @@ func mainhandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	// io.WriteString(w, "pong")
+}
+
+func authorization(w http.ResponseWriter, r *http.Request) {
+	// https://stackoverflow.com/questions/17478731/whats-the-point-of-the-x-requested-with-header
+	if xRequestedWithHeader := r.Header.Get("X-Requested-With"); xRequestedWithHeader != "XMLHttpRequest" {
+		http.Error(w, "Untrusted request", http.StatusForbidden)
+		return
+	}
+	if r.Body == nil {
+		http.Error(w, "Please send a request body", 400)
+		return
+	}
+	var auth AuthorizationStruct
+	err := json.NewDecoder(r.Body).Decode(&auth)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	fmt.Println(auth.Code)
+
+	data, err := json.Marshal(auth)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	io.WriteString(w, string(data))
 }
