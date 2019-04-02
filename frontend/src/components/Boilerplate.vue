c014884d0e7b6c2fd4441fa8f354303eedc7b0c8
louis <automatwon@gmail.com>
2019-04-02 15:35:10 -0400

    util: add component Boilerplate template

---
+++ frontend/src/components/Boilerplate.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Spinner from "@/components/Spinner.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    Spinner
+  }
+})
+export default class Boilerplate extends Vue {
+}
+</script>
+
+<style scoped lang="less">
+</style>
